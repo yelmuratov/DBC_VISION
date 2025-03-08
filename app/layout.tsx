ef6a9768df7d7251c85e52f90ebec79a27d41c2b
yelmuratov <yelimuratovsalimbay@gmail.com>
2025-03-09 02:05:00 +0500

    Favicon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,19 +11,19 @@ export default function Layout(props: { children: React.ReactNode }) {
         <link
           rel="apple-touch-icon"
           sizes="76x76"
-          href="/static/favicons/apple-touch-icon.png"
+          href="/static/images/icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/static/favicons/favicon-32x32.png"
+          href="/static/images/icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/static/favicons/favicon-16x16.png"
+          href="/static/images/icon.png"
         />
         <link rel="manifest" href="/static/favicons/manifest.json" />
       </head>
